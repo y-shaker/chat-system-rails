195e3a535e6fa36ef7cc3d7261c22738c392b6dc
[Youssef <MY_NAME@example.com>
2022-02-27 19:12:52 +0200

    migration fix

--- db/migrate/20220227164355_add_application_ref_to_chats.rb
+++
@@ -1,5 +0,0 @@
-class AddApplicationRefToChats < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :chats, :application, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_170744) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_27_170522) do
   create_table "applications", force: :cascade do |t|
-    t.string "token"
+    t.string "token", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,18 +25,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_170744) do
     t.datetime "updated_at", null: false
     t.string "application_token"
     t.integer "client_id"
+    t.integer "client_id_2_id"
     t.integer "client_2_id"
+    t.integer "messages_count"
     t.index ["application_token"], name: "index_chats_on_application_token"
     t.index ["client_2_id"], name: "index_chats_on_client_2_id"
     t.index ["client_id"], name: "index_chats_on_client_id"
+    t.index ["client_id_2_id"], name: "index_chats_on_client_id_2_id"
   end
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
-    t.integer "chat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["chat_id"], name: "index_clients_on_chat_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -49,9 +50,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_170744) do
     t.index ["client_id"], name: "index_messages_on_client_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_users_on_chat_id"
+  end
+
   add_foreign_key "chats", "clients"
   add_foreign_key "chats", "clients", column: "client_2_id"
-  add_foreign_key "clients", "chats"
+  add_foreign_key "chats", "clients", column: "client_id_2_id"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "clients"
+  add_foreign_key "users", "chats"
 end
